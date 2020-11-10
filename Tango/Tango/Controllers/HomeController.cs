12154f0fd7daee3b24276064597a319cae8a9d78
stanbianca <stanbianca611@gmail.com>
2020-11-10 16:50:13 +0200

    Added a few comments.

--- Tango/Tango/Controllers/HomeController.cs
+++ Tango/Tango/Controllers/HomeController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-//comment on line 6
+//comment on line
+//another comment
 namespace Tango.Controllers
 {
     public class HomeController : Controller
