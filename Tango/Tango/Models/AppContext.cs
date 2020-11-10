8c13ed7b58a32e86eb6d594516e2739e14187c61
stanbianca <stanbianca611@gmail.com>
2020-11-10 18:22:09 +0200

    Added Entity Framework and the database.

---
+++ Tango/Tango/Models/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Tango.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("DBConnectionString") { }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+    }
+}
