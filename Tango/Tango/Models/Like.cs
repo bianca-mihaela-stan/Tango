b180dc5a0d7e4f2b9a2cfd1b96c0254537f2399e
Stroie Mira <miramaria27@yahoo.com>
2020-11-10 17:32:08 +0200

    Second change.

---
+++ Tango/Tango/Models/Like.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Tango.Models;
+
+namespace Tango.Models
+{
+    public class Like
+    {
+        [Key]
+        public int LikeId { get; set; }
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+
+    }
+}
