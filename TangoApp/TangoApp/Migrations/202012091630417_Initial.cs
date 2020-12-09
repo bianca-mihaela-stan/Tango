6e69e808a0fd04566f09a88c3551cb37a49385c1
stanbianca <stanbianca611@gmail.com>
2020-12-09 18:46:56 +0200

    Added migrations and changed layout.

---
+++ TangoApp/TangoApp/Migrations/202012091630417_Initial.cs
@@ -0,0 +1,18 @@
+﻿namespace TangoApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Posts", "LastEditDate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Posts", "LastEditDate", c => c.DateTime(nullable: false));
+        }
+    }
+}
--- TangoApp/TangoApp/Models/Post.cs
+++ TangoApp/TangoApp/Models/Post.cs
@@ -13,7 +13,7 @@ namespace TangoApp.Models
         [Required(ErrorMessage ="Continutul postarii nu poate sa fie gol.")]
         public string Text { get; set; }
         public DateTime Date {get; set;}
-        public DateTime LastEditDate { get; set; }
+        public DateTime? LastEditDate { get; set; }
 
         public virtual ICollection<Comment> Comments { get; set; }
         public virtual ICollection<Like> Likes { get; set; }
