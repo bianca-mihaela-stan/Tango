5386a9179ae34cb6fbb96a30bd005b788c603ad0
MIRA MARIA STROIE <mira.stroie@s.unibuc.ro>
2020-12-15 17:41:20 +0200

    Added Friend model.

--- TangoApp/TangoApp/Controllers/FriendshipsController.cs
+++ TangoApp/TangoApp/Controllers/FriendshipsController.cs
@@ -12,13 +12,29 @@ namespace TangoApp.Controllers
     {
         // GET: Friendships
         public ApplicationDbContext db = new ApplicationDbContext();
+        [Authorize(Roles = "User,Editor,Admin")]
         public ActionResult Index()
         {
-            var user = db.Users.Find(User.Identity.GetUserId());
-            return View(user);
+            var currentUser = User.Identity.GetUserId();
+            var friendrequests = from req in db.Friendships
+                                 where req.User2Id == currentUser && req.Status == 0
+                                 select req;
+            ViewBag.Friendrequests = friendrequests.ToList();
+            return View();
         }
         public ActionResult Accept(int id)
         {
+
+            ///primim id-ul friendship-ului
+            //var friendship = db.Friendships.Find(id);
+            //db.Friendships.Remove(friendship);
+           // Friend newfriend = new Friend();
+            //newfriend.User1Id = friendship.User1Id;
+            //newfriend.User2Id = friendship.User2Id;
+            //newfriend.StartDate = DateTime.Now;
+            //db.Friends.Add(newfriend);
+
+            
             return View();
         }
 
---
+++ TangoApp/TangoApp/Models/Friend.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TangoApp.Models
+{
+    public class Friend
+    {
+        [Key]
+        public int FriendId { get; set; }
+
+
+        public string User1Id { get; set; }
+        [ForeignKey("User1Id")]
+        public virtual ApplicationUser User1 { get; set; }
+
+        public string User2Id { get; set; }
+        [ForeignKey("User2Id")]
+        public virtual ApplicationUser User2 { get; set; }
+
+        public DateTime StartDate { get; set; }
+    }
+}
