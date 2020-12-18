3c4ab82307e8629ace3f0d730fe2613238e4af4a
stanbianca <stanbianca611@gmail.com>
2020-12-18 11:08:25 +0200

    Minor changes.

---
+++ TangoApp/TangoApp/Scripts/Cascading-dropdown.js
@@ -0,0 +1,15 @@
+﻿function refreshCities()()
+{
+    src = $("#Country");
+    tgt = $("#Model");
+
+    tgt.empty();
+
+    $.ajax({
+        type: 'GET',
+        url: 'Home/GetModels',
+        dataType: 'json',
+        data: { brandName: src.val() },
+
+    })
+}
